400dc08ce66f3c91f8830328225634ff24244e6e
belal-allala <belalallala810@gmail.com>
2024-12-13 16:21:11 +0100

    Ajouter la fonctionnalité pour supprimer un auteur avec confirmation.

--- web/admin/index.php
+++ web/admin/index.php
@@ -104,8 +104,8 @@
                         ?>
                         <tr>
                             <td><?php echo $collaboration['id'] ?></td>
-                            <td><?php echo $collaboration['package'] ?></td>
-                            <!-- <td><?php echo $collaboration['author'] ?></td> -->
+                            <!-- <td><?php echo $collaboration['package'] ?></td> -->
+                            <td><?php echo $collaboration['author'] ?></td>
                             <td>
                                 <a href="php/modifier_collaboration.php?id=<?php echo $collaboration['id'] ?>" class="btn btn-primary btn-sm" >Modifier</a>
                                 <a href="php/delete_collaboration.php?id=<?php echo $collaboration['id'] ?>" onclick="return confirm('Vouler vous vraiment supprimer la collaboration d id <?php echo $collaboration['id'] ?>')" class="btn btn-danger btn-sm" >Supprimer</a>
