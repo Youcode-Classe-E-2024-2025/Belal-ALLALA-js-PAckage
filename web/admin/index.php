a99bbc30618f8f0a7c6e012c305444623eb8a3d3
belal-allala <belalallala810@gmail.com>
2024-12-13 16:20:03 +0100

    Ajouter la fonctionnalité pour insérer un nouvel auteur dans la base.

--- web/admin/index.php
+++ web/admin/index.php
@@ -107,8 +107,8 @@
                             <td><?php echo $collaboration['package'] ?></td>
                             <td><?php echo $collaboration['author'] ?></td>
                             <td>
-                                <a href="php/modifier_collaboration.php?id=<?php echo $collaboration['id'] ?>" class="btn btn-primary btn-sm" >Modifier</a>
-                                <!-- <a href="php/delete_collaboration.php?id=<?php echo $collaboration['id'] ?>" onclick="return confirm('Vouler vous vraiment supprimer la collaboration d id <?php echo $collaboration['id'] ?>')" class="btn btn-danger btn-sm" >Supprimer</a> -->
+                                <!-- <a href="php/modifier_collaboration.php?id=<?php echo $collaboration['id'] ?>" class="btn btn-primary btn-sm" >Modifier</a> -->
+                                <a href="php/delete_collaboration.php?id=<?php echo $collaboration['id'] ?>" onclick="return confirm('Vouler vous vraiment supprimer la collaboration d id <?php echo $collaboration['id'] ?>')" class="btn btn-danger btn-sm" >Supprimer</a>
                             </td>
                         </tr>
                         <?php
